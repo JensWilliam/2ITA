3304a6c3cc0f9c67e3f9e629021f03b0a85d536e
Jens William Egeland <jen17031@skole.telemarkfylke.no>
2024-10-10 17:08:43 +0200

    Add files via upload

---
+++ Python_Oppgaver/01-byttKopper.py
@@ -0,0 +1,34 @@
+# Laget av Jens William Egeland
+# Dato: 20/08/2024
+
+print("01 - endre inhhold i kopper")  # skriver ut oppgave-navnet 
+
+kopp1 = "binders"       # deklarerer varablene
+kopp2 = "viskelær"      # og tilordner innholdet 
+kopp3 = ""
+
+
+print("kopp1 = ", kopp1)    # skriver ut innholdet i koppene før endring
+print("kopp2 = ", kopp2)
+print("kopp3 = ", kopp3)
+
+
+kopp3 = kopp1       # bytter innhold i kopp3 og kopp1
+kopp1 = kopp2       # bytter innhold i kopp1 og kopp2
+kopp2 = kopp3       # bytter innhold i kopp2 og kopp3
+kopp3 = ""       # gir kopp3 sitt originale innhold
+
+print("etter endring:")     # skriver ut for å skille mellom innholdet før og etter bytte av innhold
+
+print("kopp1 = ", kopp1)    # skriver ut innholdet i koppene etter endring
+print("kopp2 = ", kopp2)
+print("kopp3 = ", kopp3)
+
+
+
+# hei Jens
+# Jeg har kjørt koden din og den fungerer som den skal.
+# Fortsett med det gode arbeidet
+# hilsen fra Staale
+
+
